e03b9f0827d48c5186e32c5063aa8c891d968594
Nicolò Pincelli <pincelli.n@gmail.com>
2025-01-09 14:53:01 +0100

    add comments

--- Program.cs
+++ Program.cs
@@ -2,6 +2,10 @@
 string path = "MyFile.txt";
 
 var newFile = File.CreateText(path);
+//scrivo nel documento creato
 newFile.WriteLine("Lorem");
+//chiudo la connesione con newFile
 newFile.Close();
+//elimino la variabile newFile per risparmiare risorse
 newFile.Dispose();
+
